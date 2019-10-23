4f0e43149fb95f8b4335e98103b8e2fb263fb537
Tanishq <tanishqsaluja18@gmail.com>
2019-10-23 18:39:32 +0530

    edited body parser

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware for POST request data 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Working");
